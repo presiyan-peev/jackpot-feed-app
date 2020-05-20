0c406c5760d30828e2b7015b230bca7c1c32f2dd
Presiyan Peev <pressijan@gmail.com>
2020-05-21 00:23:06 +0300

    add connection to dummy dummy API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     home
+    {{ info }}
     <BigPot />
     <MediumPot />
     <MediumPot />
@@ -12,11 +13,23 @@
 import BigPot from '@/components/BigPot.vue'
 import MediumPot from '@/components/MediumPot.vue'
 
+import axios from 'axios'
+
 export default {
+  data: () => {
+    return {
+      info: null
+    }
+  },
   name: 'Home',
   components: {
     BigPot,
     MediumPot
+  },
+  mounted () {
+    axios
+      .get('http://localhost:3000/pots')
+      .then(response => (this.info = response.data[0].type))
   }
 }
 
